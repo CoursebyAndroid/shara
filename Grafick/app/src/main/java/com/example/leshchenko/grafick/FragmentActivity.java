ad317c6863c44f563d4cbf10c4916139857452e5
Yura <lechenko.yura@gmail.com>
2017-11-16 22:05:34 +0200

    GraficRelis

--- Grafick/app/src/main/java/com/example/leshchenko/grafick/FragmentActivity.java
+++ Grafick/app/src/main/java/com/example/leshchenko/grafick/FragmentActivity.java
@@ -1,11 +1,12 @@
 package com.example.leshchenko.grafick;
 
-import android.app.Activity;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +19,19 @@ import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.OnDataPointTapListener;
 import com.jjoe64.graphview.series.Series;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 public class FragmentActivity extends Fragment {
+    private final static String TAG = " FragmentActivity ";
     private final Handler mHandler = new Handler();
     private Runnable mTimer;
+    private double timeA;
+    private Date date;
+    private final Date DATA = new Date();
     //Объкты линий
     private LineGraphSeries<DataPoint> mSeries1;
     private LineGraphSeries<DataPoint> mSeries2;
@@ -36,10 +41,12 @@ public class FragmentActivity extends Fragment {
 
     ArrayList<Double>lineA;
     ArrayList<Double>lineB;
-    private double graphValueXLineA = 2d;
-    private double graphValueXLineB = 5d;
+    private int graphValueXLineA = 2;
+    private int graphValueXLineB = 3;
     private String pm10;
     private String pm2;
+    private  int x = 1;
+    private  double timeB;
 
 
     private OnFragmentInteractionListener mListener;
@@ -64,61 +71,69 @@ public class FragmentActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment, container, false);
-        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        final GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+
+        Log.e(TAG,"Start Date =" + DATA);
+        timeA = DATA.getTime();
+        Log.e(TAG,"Start MillSec = " + timeA);
         //Текст куда записываю рандомные числа
          textPM10 = (TextView) rootView.findViewById(R.id.text_view_pm10);
          textPM2 = (TextView) rootView.findViewById(R.id.text_view_pm2);
 
         //Создаю первую линию
         mSeries1 = new LineGraphSeries<>();
-        //Делаю этой лини настройки, цвет,включение точек, радиус точки,вывод на график последние показание точки, плюс тост при нажатие на точку.
         mSeries1.setColor(Color.CYAN);
         mSeries1.setDrawDataPoints(true);
         mSeries1.setDataPointsRadius(20);
         mSeries1.setThickness(7);
-        mSeries1.setTitle("PM2 = " + pm2);
         mSeries1.setOnDataPointTapListener(new OnDataPointTapListener() {
             @Override
             public void onTap(Series series, DataPointInterface dataPoint) {
-                Toast.makeText(getActivity(), "PM5: "+dataPoint, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "PM5: " + dataPoint, Toast.LENGTH_SHORT).show();
             }
         });
+
+        Log.e(TAG,"graph Series1 " + graph.toString());
+
         //Создаю вторую линию и настройки к ней
        mSeries2 = new LineGraphSeries<>();
-
         mSeries2.setColor(Color.GREEN);
         mSeries2.setDrawDataPoints(true);
         mSeries2.setDataPointsRadius(20);
         mSeries2.setThickness(7);
-        mSeries2.setTitle("PM10 = " + pm10);
+        mSeries2.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(getActivity(), "PM10: " + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        // Значение по вертикали и горизонтали, подписи.
-        graph.getLegendRenderer().setVisible(true);
+      graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(1);
+        graph.getViewport().setMaxX(20);
+      graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(1);
+        graph.getViewport().setMaxY(1000);
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setScrollableY(true);
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter()
         {
             @Override
             public String formatLabel(double value,boolean isValueX){
-                if(isValueX){
-                    return "Min " + super.formatLabel(value,isValueX);
-                }else {
-                    return "Pm " + super.formatLabel(value, isValueX);
+                if (isValueX) {
+                    return value != 0 ? super.formatLabel(value, isValueX) + "\nSec" : super.formatLabel(0, isValueX);
+                } else {
+                    Html.fromHtml("<sup>3</sup>");
+                    return value != 0 ? super.formatLabel(value, isValueX) + "\n" + Html.fromHtml("<sup>pm</sup>") : super.formatLabel(0, isValueX);
                 }
             }
         });
-        // Добавляю в график линию 1 и 2, а также настройки графика, сролл, мин и мак значения по х и y
         graph.addSeries(mSeries1);
         graph.addSeries(mSeries2);
-        graph.getViewport().setScalable(true);
-        graph.getViewport().setScalableY(true);
-        graph.setBackgroundColor(Color.GRAY);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().isXAxisBoundsManual();
-        graph.getViewport().setMinX(1);
-        graph.getViewport().setMaxX(10);
-
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(100);
         return rootView;
     }
 
@@ -131,19 +146,25 @@ public class FragmentActivity extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        // тут происходит магия постоянной работы
-        mTimer = new Runnable() {
-            @Override
-            public void run() {
-                graphValueXLineA += 1d;
-                graphValueXLineB += 1d;
-                    mSeries1.appendData(new DataPoint(graphValueXLineA,pointYLineA()),true,40);
-                    mSeries2.appendData(new DataPoint(graphValueXLineB,pointYLineB()),true,40);
-
-                mHandler.postDelayed(this, 300);
-            }
-        };
-        mHandler.postDelayed(mTimer, 15000);
+        try {
+                mTimer = new Runnable() {
+                    @Override
+                    public void run() {
+                        date = new Date();
+                        timeB = date.getTime();
+                        graphValueXLineA = timer(timeA, timeB);
+                        graphValueXLineB = timer(timeA, timeB) - 1;
+                        Log.e(TAG,"graphValueXLineA " + graphValueXLineA);
+                        Log.e(TAG,"graphValueXLineB " + graphValueXLineB);
+                        mSeries1.appendData(new DataPoint(graphValueXLineA, pointYLineA()), false, 180);
+                        mSeries2.appendData(new DataPoint(graphValueXLineB, pointYLineB()), false, 180);
+                        mHandler.postDelayed(this, 15_000);
+                    }
+                };
+          mHandler.postDelayed(mTimer, 5_000);
+            }catch(Exception e){
+                e.printStackTrace();
+        }
     }
 
     @Override
@@ -164,15 +185,33 @@ public class FragmentActivity extends Fragment {
 
     private double pointYLineA(){
         double mLastRandom = 2;
-        mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+        mLastRandom += mRand.nextDouble()*900 - 2.25;
+        textPM2.setText(String.valueOf(mLastRandom));
         return mLastRandom;
     }
 
 
     private double pointYLineB(){
-        double mLastRandom = 20;
-         mLastRandom += mRand.nextDouble()*0.5 - 0.25;
+        double mLastRandom = 5;
+         mLastRandom += mRand.nextDouble()*500 - 0.25;
+        textPM10.setText(String.valueOf(mLastRandom));
         return mLastRandom;
     }
+    private int timer(double timeA, double timeB){
+        //double result;
+        int convertTime = 0;
+        convertTime += (timeB - timeA) / 1000;
+        Log.e(TAG,"convertTime " + convertTime);
+       // String getStringTime = formatDuration(convertTime);
+      //  result = Double.parseDouble(getStringTime);
+        return convertTime;
+    }
+
+    private static String formatDuration(long duration) {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration) % 60;
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration) % 60;
+        return String.format("%02d.%02d" , minutes, seconds);
+    }
+
     Random mRand = new Random();
 }
